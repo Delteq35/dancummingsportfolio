af210eca349e1772b893b95582ef1f74d97aa867
Delteq35 <93228009+Delteq35@users.noreply.github.com>
2022-01-03 16:38:04 +0000

    initial commit

---
+++ .eleventy.js
@@ -0,0 +1,14 @@
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addWatchTarget("./src/sass/");
+  eleventyConfig.addPassthroughCopy("./src/css");
+  eleventyConfig.addPassthroughCopy("./src/js");
+  eleventyConfig.addPassthroughCopy("./src/img");
+  eleventyConfig.addPassthroughCopy({ "**/*.jpg": "img" });
+  eleventyConfig.addPassthroughCopy({ "**/*.png": "img" });
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+    },
+  };
+};
